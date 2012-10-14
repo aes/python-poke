043531c6a7904d7b8bb56a267ae9e6c3114b7db0
Anders Eurenius <aes@nerdshack.com>
2012-10-14 18:38:08 +0200

    Adding implementations of peek and poke procedures

--- poke.c
+++ poke.c
@@ -1,6 +1,34 @@
 #include <Python.h>
 
+PyObject *peek(PyObject *self, PyObject *args)
+{
+  unsigned long long q, l;
+
+  if (!PyArg_ParseTuple(args, "KK", &q, &l))
+    return NULL; /* exception raised by PyArg_ParseTuple, NULL is returned. */
+
+  return PyString_FromStringAndSize((void *) q, l);
+}
+
+
+PyObject *poke(PyObject *self, PyObject *args)
+{
+  unsigned long long q, l;
+  const char *s;
+
+  if (!PyArg_ParseTuple(args, "Ks#", &q, &s, &l))
+    return NULL; /* exception raised by PyArg_ParseTuple, NULL is returned. */
+
+  memcpy((void *) q, s, l);
+
+  Py_INCREF(Py_None);
+  return Py_None;
+}
+
+
 PyMethodDef methods[] = {
+  {"peek", peek},
+  {"poke", poke},
   {NULL, NULL},
 };
 
